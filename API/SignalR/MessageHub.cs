429b2741c1d2b76d4265c670e3ba6f97c9fdd1d5
milo.kastablank@protonmail.com <milo.kastablank@protonmail.com>
2023-02-06 16:09:24 +0100

    Setup MessageHub, working messaging between two endusers. Need to add group tracking and notifications

---
+++ API/SignalR/MessageHub.cs
@@ -0,0 +1,97 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR;
+
+[Authorize]
+public class MessageHub : Hub
+{
+    private readonly IMessageRepository _messageRepository;
+    private readonly IUserService _userService;
+    private readonly IMessageService _messageService;
+    private readonly IMapper _mapper;
+
+    public MessageHub(IMessageRepository messageRepository, IUserService userService, IMessageService messageService,
+        IMapper mapper)
+    {
+        _messageRepository = messageRepository;
+        _userService = userService;
+        _messageService = messageService;
+        _mapper = mapper;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+        if (httpContext != null)
+        {
+            var otherUser = httpContext.Request.Query["user"];
+            var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var messages = await _messageRepository
+                .GetMessageThread(Context.User.GetUsername(), otherUser);
+
+            await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+        }
+    }
+
+    public override Task OnDisconnectedAsync(Exception exception)
+    {
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task SendMessage(CreateMessageDto createMessageDto)
+    {
+        var (senderUsername, recipientUsername) = ValidateSenderAndRecipient(createMessageDto);
+        var sender = await GetSender(senderUsername);
+        var recipient = await GetRecipient(recipientUsername);
+        var message = _messageService.AddMessage(createMessageDto, sender, recipient);
+        await SaveChangesAndNotifyRecipients(message, sender, recipient);
+    }
+
+    private async Task<AppUser> GetSender(string senderUsername)
+    {
+        return await _userService.GetUserByUsername(senderUsername);
+    }
+
+
+    private async Task<AppUser> GetRecipient(string recipientUsername)
+    {
+        var recipient = await _userService.GetUserByUsername(recipientUsername);
+        if (recipient is null) throw new HubException("Recipient not found");
+
+        return recipient;
+    }
+
+    private (string senderUsername, string recipientUsername) ValidateSenderAndRecipient(
+        CreateMessageDto createMessageDto)
+    {
+        var senderUsername = Context.User.GetUsername();
+        var recipientUsername = createMessageDto.RecipientUsername;
+        if (senderUsername == recipientUsername.ToLower())
+            throw new HubException("You cannot send messages to yourself");
+
+        return (senderUsername, recipientUsername);
+    }
+
+    private async Task SaveChangesAndNotifyRecipients(Message message, AppUser sender, AppUser recipient)
+    {
+        if (await _messageService.SaveAllMessagesAsync())
+        {
+            var group = GetGroupName(sender.UserName, recipient.UserName);
+            await Clients.Group(group).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
+        }
+    }
+
+    private string GetGroupName(string caller, string other)
+    {
+        var stringCompare = string.CompareOrdinal(caller, other) < 0;
+        return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+    }
+}
---
+++ API/SignalR/PresenceTracker.cs
@@ -0,0 +1,51 @@
+﻿namespace API.SignalR;
+
+public class PresenceTracker
+{
+    public static readonly Dictionary<string, List<string>> OnlineUsers = new();
+
+    public Task UserConnected(string username, string connectionId)
+    {
+        // Put a lock because a Dict is not thread safe. So with lock we make it un-async
+        lock (OnlineUsers)
+        {
+            if (OnlineUsers.ContainsKey(username))
+            {
+                OnlineUsers[username].Add(connectionId);
+            }
+            else
+            {
+                OnlineUsers.Add(username, new List<string> {connectionId});
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task UserDisconnected(string username, string connectionId)
+    {
+        lock (OnlineUsers)
+        {
+            if (!OnlineUsers.ContainsKey(username)) return Task.CompletedTask;
+            OnlineUsers[username].Remove(connectionId);
+
+            if (OnlineUsers[username].Count == 0)
+            {
+                OnlineUsers.Remove(username);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task<string[]> GetOnlineUsers()
+    {
+        string[] onlineUsers;
+        lock (OnlineUsers)
+        {
+            onlineUsers = OnlineUsers.OrderBy(k => k.Key).Select(k => k.Key).ToArray();
+;        }
+
+        return Task.FromResult(onlineUsers);
+    }
+}
